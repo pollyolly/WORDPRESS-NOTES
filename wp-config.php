a8e0dad52b2c20d5978cde54639854c0b86e5b83
John Mark <johnmarkroco05@gmail.com>
2020-06-03 22:33:43 +0800

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -4,6 +4,10 @@
 //define('FORCE_SSL_LOGIN', true);
 //define('FORCE_SSL_ADMIN', true);
 //define('WP_ALLOW_REPAIR', true);
+
+define('WP_HOME', 'https://labs.dilc.upd.edu.ph/ilc_blogsite');
+define('WP_SITEURL', 'https://labs.dilc.upd.edu.ph/ilc_blogsite');
+
 define('DISALLOW_FILE_EDIT', true);
 define('WP_POST_REVISIONS', 10); 
 define('EMPTY_TRASH_DAYS', 7);
